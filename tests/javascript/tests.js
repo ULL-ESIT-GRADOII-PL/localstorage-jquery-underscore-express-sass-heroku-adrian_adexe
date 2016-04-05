05fc389a90af0cae51f82b39aed612e36330f0bd
Adexe Sabina <alu0100769609@ull.edu.es>
2016-04-05 15:56:24 +0100

    Arreglado el test que fallaba

--- tests/javascript/tests.js
+++ tests/javascript/tests.js
@@ -1,8 +1,8 @@
 var expect = chai.expect;
 
 describe("Comprobación CSV dobles comillas", function(){
-   it("Debe poner los siguiente en un mismo cuadro", function(){
-      var r = calculate('"Esto en una misma columna",23');
-      expect(r[0]).to.equal('"Esto en una misma columna"');
+   it("Debe poner el texto en un mismo cuadro", function(){
+      var r = calculate('"Esto en una misma columna"');
+      expect(r[0].value[0]).to.equal('Esto en una misma columna');
    });
 });
