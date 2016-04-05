938f68a5dea3ec526ce5ed8bdb186cea779a013d
Adexe Sabina <alu0100769609@ull.edu.es>
2016-04-05 13:14:47 +0100

    Update 2016-04-05T12:14:44.854Z

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ gutil   = require('gulp-util');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src(['./*.js', './*.css', './*.txt', './*.html', './*.md', 'tests/*'])
+  return gulp.src(['./*.js', './*.css', './*.txt', './*.html', './*.md', 'tests/**'])
     .pipe(ghPages());
  });
